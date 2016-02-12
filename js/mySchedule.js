724f5a6e74037ce81fb667d63e1c36e209a9c05a
Chao Pan <c5pan@ucsd.edu>
2016-02-12 00:56:38 -0800

    testing pull

--- js/mySchedule.js
+++ js/mySchedule.js
@@ -1,76 +1,229 @@
+/* ------------------global variables (sample json)----------------- */
+var jsonObject = '{ "friend1" : { \
+                                "mon" : [0,1,2,4,5,8], \
+                                "tue" : [0,3,4,5], \
+                                "wed" : [1,4,2], \
+                                "thu" : [8], \
+                                "fri" : [7], \
+                                "sat" : [6], \
+                                "sun" : [5]  \
+                                }, \
+                    "friend2" : { \
+                                "mon" : [5,6,9], \
+                                "tue" : [0,1,2], \
+                                "wed" : [1,4,2], \
+                                "thu" : [8], \
+                                "fri" : [7], \
+                                "sat" : [6], \
+                                "sun" : [5] \
+                                } \
+                }';
+//var data = JSON.parse(jsonObject);
+sessionStorage.setItem('example', jsonObject);
+var exampleData = sessionStorage.getItem('example');
+var data = JSON.parse(exampleData);
 
-// Call this function when the page loads (the "ready" event)
+
+                
+/* ---------- Page Load Function --------------*/
 $(document).ready(function() {
-	initializePage();
-    fillBlocks("mon");
+	console.log("Javascript connected!");
+    
+    drawTable();
+    //first draw monday because default
+    generateTab("mon-tab");
+    generateTab("tue-tab");
+    generateTab("wed-tab");
+    generateTab("thu-tab");
+    generateTab("fri-tab");
+    generateTab("sat-tab");
+    generateTab("sun-tab");
+    
+    console.log("html is:");
+    console.log($(".time-table td").html());
+    if($(".time-table td").html() == "placeholder"){
+       $(".time-table td").empty();
+       $(".time-table td").html("</br>");
+    }
+    else{
+        $(".time-table td").append("</br>");
+    }
+    
+    
+    //on tab switch, draw according to the day of tab
     $(document).on( 'shown.bs.tab', 'a[data-toggle="pill"]', function (e) {
     console.log(e.target.id);
-    showTab(e);
     });
-    
+
 });
 
+//angular stuff
+/* var app = angular.module('schedule', []);
+app.directive("scheduleTemplate", function() {
+    return {
+        template: timeHtml
+    }
+}); */
+
+//handlebars stuff
+function drawTable(){
+    //use handlebars to generate a table
+    var source = $("#table-template").html();
+    var template = Handlebars.compile(source);
+    var context = { //move this to a different file or something.
+        timeSlots : timeContentDebug
+    }
+    var new_html = template(context);
+    $("#monday").html(new_html);
+    $("#tuesday").html(new_html);
+    $("#wednesday").html(new_html);
+    $("#thursday").html(new_html);
+    $("#friday").html(new_html);
+    $("#saturday").html(new_html);
+    $("#sunday").html(new_html);
+}
 
 
-function initializePage() {
-	console.log("Javascript connected!");
-    //$("#tuesday").html(timeHtml);
-    $("#wednesday").html(timeHtml);
+
+
+
+//keep this for testing
+function fillBlocks(day){
+    //sample array:
+    //var array = [0,1,3,5];
+    console.log("in fillblocks");
+    
+    var prefix = ".tab-content .active "
+        /*debug*/ var testname = $(prefix).attr('class'); console.log(testname);
+    //fill block by user : start with just 1 schedule
+    for(var i = 0 ; i < array.length; i++){
+        var id = prefix + "#t" + array[i]; // ".tab-content .active #t3"
+        console.log("id = " + id);
+            /*debug*/ var name = $(id).attr('id');  console.log(name);
+        $(id).css("background-color", "rgba(0,0,255,0.20)");
     }
     
+}
 
-function showTab(e){
+function drawBlocks(){
     
-    var daytab = e.target.id;
+}
+
+
+
+function fillBlocks(day, color, times){
+    //day is "mon", "tues", "wed", ...
+    //color is "red", "yellow", "blue", ...
+    //times is [0,1,3,4,5]
+    
+    var prefix = ".tab-content #" + day;
+    for(var i = 0 ; i < times.length; i++){
+        var id = prefix + " #t" + times[i]; // ".tab-content # #t3"
+            //console.log("id = " + id);
+            /*debug*/ console.log(id);
+            /*debug*/ var name = $(id).attr('id');
+        //get current color and turn it into an array
+/*         var currentColor = $(id).css("background-color"); //get 'rgba(r,g,b,a)
+        var rgba = currentColor.substring(5, currentColor.length-1).split(/(?:,| )+/); //becomes [r,g,b,a]
+        console.log(rgba);
+        var rgbaValues = [parseInt(rgba[0]),parseInt(rgba[1]),parseInt(rgba[2]),parseFloat(rgba[3])];
+        console.log(rgbaValues);
+        
+        //decide on 2nd color
+        var rgbaValues2 = [255,0,0,0.3]
+        if(color == 1){ 
+            rgbaValues2 = [0,0,255,0.3];
+            }
+        else if(color == 2){ 
+            rgbaValues2 = [0,255,0,0.3];
+            }
+        
+        //plug in 1st and 2nd color to get combined color
+        rgbaValues = addColors(rgbaValues, rgbaValues2);
+        console.log(rgbaValues);
+        
+        //make it into a string again
+        rgba = "rgba(" + rgbaValues[0] + "," + rgbaValues[1] + "," + rgbaValues[2] + "," + rgbaValues[3] + ")"
+        console.log("final rgba = " + rgba);
+        
+        $(id).css("background-color", rgba); //drawing stuff
+         */
+        //create a div inside
+        
+        if($(id).html() == "placeholder"){
+            $(id).html("");
+        };
+        $(id).append("<div class='stack-color" + color + "'> &nbsp;</div>");
+        
+
+        
+        if(i == 0){
+            //draw the top-left and top-right border
+            $(id).css("border-top-left-radius", "10px");
+            $(id).css("border-top-right-radius", "10px");
+        }
+        else if( times[i-1] != times[i] - 1 ){
+            //draw the top-left and top-right border
+            $(id).css("border-top-left-radius", "10px");
+            $(id).css("border-top-right-radius", "10px");
+        }
+        if( i < times.length - 1 && times[i + 1] == times[i] + 1){
+            //$(id).css("border-bottom-left-radius", "10px");
+            //$(id).css("border-bottom-right-radius", "10px");
+        }
+        if(i == times.length - 1){
+            $(id).css("border-bottom-left-radius", "10px");
+            $(id).css("border-bottom-right-radius", "10px");
+        }
+        
+    }
+}
+
+function outlineBlocks(day,times){
+    
+    
+}
+
+function generateTab(daytab){
+    //TODO: split the data up into friend1, friend2
+    
+    //use "mon-tab" to get the corresponding day array from JSON object
+    //var array = parseSchedules(daytab);
+    var day = "mon-tab";
+    console.log(data);
+    console.log(data["friend1"]);
+    console.log(data["friend1"]["mon"]);
+    //check if the array from JSON object has blocks
+
     console.log(daytab);
-    if(daytab == "mon-tab"){
-        fillBlocks("mon");
-    }
-    if(daytab == "tue-tab"){
-        if( $(".tab-content .active").html() == '0'){
-            console.log("filling table");
-            $("#tuesday").html(timeHtml);
-        }
-        fillBlocks("tue");
-    }
-    if(daytab == "wed-tab"){
-        if( $(".tab-content .active").html() == '0'){
-            console.log("filling table");
-            $("#wednesday").html(timeHtml);
-        }
-        fillBlocks("wed");
-    }
-    if(daytab == "thu-tab"){
-        if( $(".tab-content .active").html() == '0'){
-            console.log("filling table");
-            $("#thursday").html(timeHtml);
-        }
-        fillBlocks("thu");
-    }
-    if(daytab == "fri-tab"){
-        if( $(".tab-content .active").html() == '0'){
-            console.log("filling table");
-            $("#friday").html(timeHtml);
-        }
-        fillBlocks("fri");
-    }
     
-    if(daytab == "sat-tab"){
-        if( $(".tab-content .active").html() == '0'){
-            console.log("filling table");
-            $("#saturday").html(timeHtml);
-        }
-        fillBlocks("sat");
+    //call fillBlocks function 
+    switch(daytab){
+        case "mon-tab" : day = "monday"; break; 
+        case "tue-tab" : day = "tuesday"; break;
+        case "wed-tab" : day = "wednesday"; break;
+        case "thu-tab" : day = "thursday"; break;
+        case "fri-tab" : day = "friday"; break;
+        case "sat-tab" : day = "saturday"; break;
+        case "sun-tab" : day = "sunday"; break;
+        default : console.log("error: invalid day");
     }
-    
-    if(daytab == "sun-tab"){
-        if( $(".tab-content .active").html() == '0'){
-            console.log("filling table");
-            $("#sunday").html(timeHtml);
+    var colorNum = 0;
+    var shortenedDay = daytab.substring(0,3);
+    console.log(shortenedDay);
+    for(var friend in data){
+        if(data[friend][shortenedDay].length == 0){
+            console.log("no blocks need coloring");
+            colorNum++;
         }
-        fillBlocks("sun");
+        else{
+            fillBlocks(day,colorNum,data[friend][shortenedDay]);
+            colorNum++;
+        }
     }
+        
 
+    
 }
 
 var timeHtml = "<table class = 'time-table'> \
@@ -124,66 +277,60 @@ var timeHtml = "<table class = 'time-table'> \
                 "sun" : []}
 }
 */
-var jsonObject = '{ "friend" : { \
-                "mon" : [0,1,2,5,9,10], \
-                "tue" : [0,3,4,5], \
-                "wed" : [1,4,4,2], \
-                "thu" : [8], \
-                "fri" : [7], \
-                "sat" : [6], \
-                "sun" : [5] } \
-                }';
+
+                
+                
+function addColors(rgba1,rgba2){ //uses math to overlay 2 colors
+    var r1 = rgba1[0];
+    var g1 = rgba1[1];
+    var b1 = rgba1[2];
+    var a1 = rgba1[3];
+    var r2 = rgba2[0];
+    var g2 = rgba2[1];
+    var b2 = rgba2[2];
+    var a2 = rgba2[3];
+    var aN = (a2 + a1*(1-a2))
+    var rN = Math.floor( (r2*a2 + r1*a1*(1-a1)) / aN );
+    var gN = Math.floor( (g2*a2 + g1*a1*(1-a1)) / aN );
+    var bN = Math.floor( (b2*a2 + b1*a1*(1-a1)) / aN );
+    aN = (aN + 0.10).toFixed(2);
+    var newRGBA = [rN,gN,bN, aN ];
+    return newRGBA;
+}
 
 
-function parseSchedules(day){
+
+//unused function
+function parseSchedules(day){ //should have friend, day as parameters
     //extracts all json objects from session.
+    //returns array
     var obj = JSON.parse(jsonObject);
-    if(day === "mon"){
-        console.log(obj.friend.mon);
-        return obj.friend.mon;
+    if(day === "mon-tab"){
+        console.log(obj.friend1.mon);
+        return obj.friend1.mon;
     }
-    else if (day === "tue"){
+    else if (day === "tue-tab"){
         return obj.friend.tue;
     }
     //var array = [obj.friend.mon, obj.friend.tue, obj.friend.wed]; //TODO: expand
-    else if (day === "wed"){
+    else if (day === "wed-tab"){
         return obj.friend.wed;
     }
-    else if (day === "thu"){
+    else if (day === "thu-tab"){
         return obj.friend.thu;
     }
-    else if (day === "fri"){
+    else if (day === "fri-tab"){
         return obj.friend.fri;
     }
-    else if (day === "sat"){
+    else if (day === "sat-tab"){
         return obj.friend.sat;
     }
-    else if (day === "sun"){
+    else if (day === "sun-tab"){
         return obj.friend.sun;
     }
-    return -1;
+    return [];
 }
-
-
-function fillBlocks(day){
-    //sample array:
-    //var array = [0,1,3,5];
-    console.log("in fillblocks");
-    var array = parseSchedules(day);
-    var prefix = ".tab-content .active "
-        /*debug*/ var testname = $(prefix).attr('class'); console.log(testname);
-    //fill block by user : start with just 1 schedule
-    for(var i = 0 ; i < array.length; i++){
-        var id = prefix + "#t" + array[i]; // ".tab-content .active #t3"
-        console.log("id = " + id);
-            /*debug*/ var name = $(id).attr('id');  console.log(name);
-        $(id).css("background-color", "rgba(0,0,255,0.20)");
-       
-    }
-    
-}
-
-document.getElementById('eventblock').appendChild(table);
+//document.getElementById('eventblock').appendChild(table);
 function clickfunction(e){
     e.preventDefault();
     console.log("clicked");
---
+++ js/table.js
@@ -0,0 +1,215 @@
+var timeContentDebug = [{timeIDAbove: "this-is-empty", 
+                      timeContentAbove: "&nbsp;", 
+                      timeLabel: "7:00 Am",
+                      timeIDBelow: "t0", 
+                      timeContentBelow: "7"
+                      },
+                      
+                      {timeIDAbove: "t1", 
+                      timeContentAbove: "730", 
+                      timeLabel: "8:00 Am",
+                      timeIDBelow: "t2", 
+                      timeContentBelow: "8"
+                      },
+                      
+                      {timeIDAbove: "t3", 
+                      timeContentAbove: "830", 
+                      timeLabel: "9:00 Am",
+                      timeIDBelow: "t4", 
+                      timeContentBelow: "9"
+                      },
+                      
+                      {timeIDAbove: "t5", 
+                      timeContentAbove: "930", 
+                      timeLabel: "10:00 Am",
+                      timeIDBelow: "t6", 
+                      timeContentBelow: "10"
+                      },
+                      
+                      {timeIDAbove: "t7", 
+                      timeContentAbove: "1030", 
+                      timeLabel: "11:00 Am",
+                      timeIDBelow: "t8", 
+                      timeContentBelow: "11"
+                      },
+                      
+                      {timeIDAbove: "t9", 
+                      timeContentAbove: "1130", 
+                      timeLabel: "12:00 Pm",
+                      timeIDBelow: "t10", 
+                      timeContentBelow: "12"
+                      },
+                      
+                      {timeIDAbove: "t11", 
+                      timeContentAbove: "1230", 
+                      timeLabel: "1:00 Pm",
+                      timeIDBelow: "t12", 
+                      timeContentBelow: "1"
+                      },
+                      
+                      {timeIDAbove: "t13", 
+                      timeContentAbove: "130", 
+                      timeLabel: "2:00 Pm",
+                      timeIDBelow: "t14", 
+                      timeContentBelow: "2"
+                      },
+                      
+                      {timeIDAbove: "t15", 
+                      timeContentAbove: "230", 
+                      timeLabel: "3:00 Pm",
+                      timeIDBelow: "t16", 
+                      timeContentBelow: "3"
+                      },
+                      
+                      {timeIDAbove: "t17", 
+                      timeContentAbove: "330", 
+                      timeLabel: "4:00 Pm",
+                      timeIDBelow: "t18", 
+                      timeContentBelow: "4"
+                      },
+                      
+                      {timeIDAbove: "t19", 
+                      timeContentAbove: "430", 
+                      timeLabel: "5:00 Pm",
+                      timeIDBelow: "t20", 
+                      timeContentBelow: "5"
+                      },
+                      {timeIDAbove: "t21", 
+                      timeContentAbove: "530", 
+                      timeLabel: "6:00 Pm",
+                      timeIDBelow: "t22", 
+                      timeContentBelow: "6"
+                      },
+                      {timeIDAbove: "t23", 
+                      timeContentAbove: "630", 
+                      timeLabel: "7:00 Pm",
+                      timeIDBelow: "t24", 
+                      timeContentBelow: "7"
+                      },
+                      {timeIDAbove: "t25", 
+                      timeContentAbove: "730", 
+                      timeLabel: "8:00 Pm",
+                      timeIDBelow: "t26", 
+                      timeContentBelow: "8"},
+                      
+                      {timeIDAbove: "t27", 
+                      timeContentAbove: "830", 
+                      timeLabel: "9:00 Pm",
+                      timeIDBelow: "t28", 
+                      timeContentBelow: "9"},
+                      
+                      {timeIDAbove: "t29", 
+                      timeContentAbove: "930", 
+                      timeLabel: "10:00 Pm",
+                      timeIDBelow: "t30", 
+                      timeContentBelow: "10"},
+                      ];
+
+                      
+                      
+var timeContent = [{timeIDAbove: "this-is-empty", 
+                      timeContentAbove: "placeholder", 
+                      timeLabel: "7:00 Am",
+                      timeIDBelow: "t0", 
+                      timeContentBelow: "placeholder"
+                      },
+                      
+                      {timeIDAbove: "t1", 
+                      timeContentAbove:"placeholder", 
+                      timeLabel: "8:00 Am",
+                      timeIDBelow: "t2", 
+                      timeContentBelow: "placeholder"
+                      },
+                      
+                      {timeIDAbove: "t3", 
+                      timeContentAbove: "placeholder", 
+                      timeLabel: "9:00 Am",
+                      timeIDBelow: "t4", 
+                      timeContentBelow: "placeholder"
+                      },
+                      
+                      {timeIDAbove: "t5", 
+                      timeContentAbove: "placeholder", 
+                      timeLabel: "10:00 Am",
+                      timeIDBelow: "t6", 
+                      timeContentBelow: "placeholder"
+                      },
+                      
+                      {timeIDAbove: "t7", 
+                      timeContentAbove: "placeholder", 
+                      timeLabel: "11:00 Am",
+                      timeIDBelow: "t8", 
+                      timeContentBelow: "placeholder"
+                      },
+                      
+                      {timeIDAbove: "t9", 
+                      timeContentAbove: "placeholder", 
+                      timeLabel: "12:00 Pm",
+                      timeIDBelow: "t10", 
+                      timeContentBelow: "placeholder"
+                      },
+                      
+                      {timeIDAbove: "t11", 
+                      timeContentAbove: "placeholder", 
+                      timeLabel: "1:00 Pm",
+                      timeIDBelow: "t12", 
+                      timeContentBelow: "placeholder"
+                      },
+                      
+                      {timeIDAbove: "t13", 
+                      timeContentAbove: "placeholder", 
+                      timeLabel: "2:00 Pm",
+                      timeIDBelow: "t14", 
+                      timeContentBelow: "placeholder"
+                      },
+                      
+                      {timeIDAbove: "t15", 
+                      timeContentAbove: "placeholder", 
+                      timeLabel: "3:00 Pm",
+                      timeIDBelow: "t16", 
+                      timeContentBelow: "placeholder"
+                      },
+                      
+                      {timeIDAbove: "t17", 
+                      timeContentAbove: "placeholder", 
+                      timeLabel: "4:00 Pm",
+                      timeIDBelow: "t18", 
+                      timeContentBelow: "placeholder"
+                      },
+                      
+                      {timeIDAbove: "t19", 
+                      timeContentAbove: "placeholder", 
+                      timeLabel: "5:00 Pm",
+                      timeIDBelow: "t20", 
+                      timeContentBelow: "placeholder"
+                      },
+                      {timeIDAbove: "t21", 
+                      timeContentAbove: "placeholder", 
+                      timeLabel: "6:00 Pm",
+                      timeIDBelow: "t22", 
+                      timeContentBelow: "placeholder"
+                      },
+                      {timeIDAbove: "t23", 
+                      timeContentAbove: "placeholder", 
+                      timeLabel: "7:00 Pm",
+                      timeIDBelow: "t24", 
+                      timeContentBelow: "placeholder"
+                      },
+                      {timeIDAbove: "t25", 
+                      timeContentAbove: "placeholder", 
+                      timeLabel: "8:00 Pm",
+                      timeIDBelow: "t26", 
+                      timeContentBelow: "placeholder"},
+                      
+                      {timeIDAbove: "t27", 
+                      timeContentAbove: "placeholder", 
+                      timeLabel: "9:00 Pm",
+                      timeIDBelow: "t28", 
+                      timeContentBelow: "placeholder"},
+                      
+                      {timeIDAbove: "t29", 
+                      timeContentAbove: "placeholder", 
+                      timeLabel: "10:00 Pm",
+                      timeIDBelow: "t30", 
+                      timeContentBelow: "placeholder"},
+                      ];
