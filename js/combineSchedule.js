0df01a5280157030e30ea1660e2ebca27284bc38
Chao Pan <c5pan@ucsd.edu>
2016-02-12 06:39:35 -0800

    hard-coded friend2 works. registering still broken

--- js/combineSchedule.js
+++ js/combineSchedule.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-
+    
 })
 
 /*
@@ -26,7 +26,41 @@ var jsonObject = '{ "friend1" : { \
 */
 
 function combineFunction() {
+    //this is hardcoded, make this not hardcoded later
+    if(sessionStorage.getItem('example') === null){
+    console.log("no data in session storage");
+    var jsonObject = '{ "friend1" : { \
+                            "mon" : [], \
+                            "tue" : [], \
+                            "wed" : [], \
+                            "thu" : [], \
+                            "fri" : [], \
+                            "sat" : [], \
+                            "sun" : []  \
+                             } \
+                       }'
+    console.log("created new jsonObject");
+    sessionStorage.setItem('example',jsonObject)
+    var exampleData = sessionStorage.getItem('example',jsonObject);
+    }
+    else{
+        var exampleData = sessionStorage.getItem('example');
+    }
+    console.log("exampleData = " + exampleData);
+    console.log(typeof(exampleData));
+    var friendToCombineWith = ' , "friend2" : { \
+                                "mon" : [0,1,2,5,6,7,10,11,12,13], \
+                                "tue" : [0,3,4,5], \
+                                "wed" : [1,4,2], \
+                                "thu" : [8], \
+                                "fri" : [7], \
+                                "sat" : [6], \
+                                "sun" : [5]  \
+                                }';
+    var newData = exampleData.substring(0, exampleData.length - 1) + friendToCombineWith + '}'; //appending strings
+    console.log(newData);
     
+    sessionStorage.setItem('example' , newData);
     /*var email = $('#combineEmail');
     
     if(email != "friend1") {
@@ -44,6 +78,8 @@ function combineFunction() {
                                 "sun" : [5]  \
                                 },';
     }*/
+    
+
     
     
     var jsonObject = '{ "friend1" : { \
@@ -65,6 +101,6 @@ function combineFunction() {
                                 "sun" : [5] \
                                 } \
                 }';
-    
-    sessionStorage.schedules = jsonObject;
+                
+    //sessionStorage.setItem('example', newData);
 }
