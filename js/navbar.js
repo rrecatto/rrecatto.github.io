117020ffafdfd950a8460135352daf7f84a50f15
Robert Recatto <rrecatto@ucsd.edu>
2016-02-11 18:07:38 -0800

    added handlebars to all files, and injected navbar
    Injected navbar in every page using javascript. Also hooked up
    handlebars script to every page as well. navbar stored in navbar.js

---
+++ js/navbar.js
@@ -0,0 +1,20 @@
+$(document).ready(function() {
+	initializePage();
+})
+
+function initializePage() {
+    
+    var htmlStr = '<nav class="navbar navbar-default">\
+          <div class="container-fluid">\
+              <ul class="navUL">\
+                  <li class="navLI"><a href="createSchedule.html">Create</a></li>\
+                  <li class="navLI"><a href="combineSchedule.html">Combine</a></li>\
+                  <li class="navLI"><a href="mySchedule.html">Name</a></li>\
+                  <li class="navLI"><button\ onclick="window.location="http://rrecatto.github.io;" type="button" class="navbar-btn">Log Out</button></li>\
+              </ul>\
+          </div>\
+      </nav>';
+
+    console.log(htmlStr);
+    $("#navbar").html(htmlStr);    
+}
