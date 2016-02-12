c4f7c60d0c7f24fc892481997411d15054d181db
Robert Recatto <rrecatto@ucsd.edu>
2016-02-12 00:04:36 -0800

    Basic content combine page, login validation
    login works for session if user created in same session. basic error
    messages on login. Basic content combine page not yet functional.

---
+++ js/login.js
@@ -0,0 +1,62 @@
+$(document).ready(function() {
+	initializePage();
+})
+
+
+function initializePage() {
+    
+
+}
+
+
+function signUpUser() {
+
+    var userName = $('#regEmail').val();
+    var passWord = $('#regPassword').val();
+    var pwRepeat = $('#regPasswordAgain').val();
+    
+    if(passWord==='' || pwRepeat==='' || userName==='') {
+        $('#regError').html('<p style="color:red;text-align:center;">ERROR: PLEASE FILL OUT ALL FIELDS</p>');
+        console.log("here");
+    }
+    else if(!(pwRepeat === passWord)) {
+        console.log("here2");
+        $('#regError').html('<p style="color:red;text-align:center;">ERROR: PASSWORDS DO NOT MATCH</p>');
+    }
+    else {
+        
+        var user = { 
+            "email": userName,
+            "pw": passWord
+        }
+        
+        sessionStorage.setItem("user", user);
+        
+        window.location.href = 'rrecatto.github.io';
+    }
+        
+}
+
+
+function authorizeLogin() {
+    
+    var userName = $('#inputEmail').val();
+    var passWord = $('#inputPassword').val();
+    var user = sessionStorage.getItem("user");
+    
+    if(userName==='' || passWord==='') {
+        $('#logError').html('<p style="color:red;text-align:center;">ERROR: PLEASE ENTER BOTH EMAIL AND PASSWORD</p>');
+        console.log("here");
+    }
+    else if(user==null) {
+        $('#logError').html('<p style="color:red;text-align:center;">ERROR: EITHER USERNAME OR PASSWORD INCORRECT (no users)</p>');
+    }
+    else if(userName!=user['email'] || passWord!=user['pw']){
+        $('#logError').html('<p style="color:red;text-align:center;">ERROR: EITHER USERNAME OR PASSWORD INCORRECT</p>');
+    }
+    else {
+        window.location.href = 'http://rrecatto.github.io/mySchedule';
+    }
+    
+           
+}
