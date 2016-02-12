a91a6b9e3e4a1d7ee37de6e8f389a01bc446556f
Robert Recatto <rrecatto@ucsd.edu>
2016-02-12 11:21:27 -0800

    fixed navbar
    added logout function in navbar to correctly redirect

--- js/navbar.js
+++ js/navbar.js
@@ -1,20 +1,16 @@
 $(document).ready(function() {
-	initializePage();
+	initializeNav();
 })
 
-function initializePage() {
+function initializeNav() {
     
-    var htmlStr = '<nav class="navbar navbar-default">\
-          <div class="container-fluid">\
-              <ul class="navUL">\
-                  <li class="navLI"><a href="createSchedule.html">Create</a></li>\
-                  <li class="navLI"><a href="combineSchedule.html">Combine</a></li>\
-                  <li class="navLI"><a href="mySchedule.html">Name</a></li>\
-                  <li class="navLI"><button onclick="window.location="http://rrecatto.github.io;" type="button" class="navbar-btn">Log Out</button></li>\
-              </ul>\
-          </div>\
-      </nav>';
+    var htmlStr = '<nav class="navbar navbar-default"><div class="container-fluid"><ul class="navUL"><li class="navLI"><a href="createSchedule.html">Create</a></li><li class="navLI"><a href="combineSchedule.html">Combine</a></li><li class="navLI"><a href="mySchedule.html">Name</a></li><li class="navLI"><button onclick="logout()" class="navbar-btn">Log Out</button></li></ul></div></nav>';
 
-    console.log(htmlStr);
     $("#navbar").html(htmlStr);    
+}
+
+function logout() {
+ 
+    window.location.href = 'rrecatto.github.io';
+    
 }
