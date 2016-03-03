fef76ce6d35c688e0d116cd28789222b0990b9b2
guruigang <r1gu@ucsd.edu>
2016-03-03 01:46:47 -0800

    Username must contain only letters, numbers

--- js/login.js
+++ js/login.js
@@ -32,7 +32,7 @@ function signUpUser() {
 
         $('#regError').html('<p style="color:red;text-align:center;">ERROR: USERNAME ALREADY EXIST</p>');
         
-    }else if(!userName.match(letters)) {
+    }else if(!userName.substr(0,1).match(letters)) {
          $('#regError').html('<p style="color:red;text-align:center;">ERROR: USERNAME CAN ONLY BE LETTERS</p>');
     }else if (!re.test(userName.substr(1))) {
     	$('#regError').html('<p style="color:red;text-align:center;">ERROR:Username must contain only letters, numbers and underscores!</p>');	
